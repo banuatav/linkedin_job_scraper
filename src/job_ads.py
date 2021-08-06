8654211bf860a2df05ed2ffa32e73b0f2deba5c5
Banu Atav <atavbanu@gmail.com>
2021-08-06 12:21:39 +0200

    ADD JobAds class

--- src/job_ads.py
+++ src/job_ads.py
@@ -4,6 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from bs4 import BeautifulSoup
+
+import logging
+logger = logging.getLogger(__name__)
+
 _MORE_BUTTON_XPATH = "//footer/button[@aria-label='Click to see more description']"
 _SELECTION_AREA = "grid"
 
@@ -21,6 +26,7 @@ class JobAd():
 
         if self.scrape:
             self.scrape_page(link)
+            self.extract_info()
 
         if self.quit_driver:
             self.driver.quit()
@@ -58,6 +64,45 @@ class JobAd():
             print("Couldn't find selection area'")
 
         time.sleep(2)
+
+    def extract_info(self):
+        soup = BeautifulSoup(self.source_code, "html.parser")
+
+        # Extract job name
+        try:
+            self.job_name = soup.find(
+                "div", {"class": "jobs-unified-top-card t-14 artdeco-card mb4"})
+        except:
+            self.job_name = None
+            logger.warning("Couldn't extract job name.")
+
+        # Extract job description
+        try:
+            self.job_descr = soup.find("div", {"id": "job-details"})
+        except:
+            logger.warning("Couldn't extract job description.")
+
+        # Extract job poster
+        try:
+            self.job_poster = soup.find(
+                "div", {"class": "jobs-description__details"})
+        except:
+            self.job_poster = None
+            logger.warning("Couldn't extract job poster.")
+
+        # Extract job salary
+        try:
+            self.job_salary = soup.find("div", {"id": "SALARY"})
+        except:
+            self.job_salary = None
+            logger.warning("Couldn't extract job salary.")
+
+        # Extract job company
+        try:
+            self.job_company = soup.find("div", {"class": "jobs-company__box"})
+        except:
+            self.job_company = None
+            logger.warning("Couldn't extract job company.")
 
     def __repr__(self):
         return "\n\n".join(self.scraping_results)
