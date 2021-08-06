eaa40aa384064de8b618dbcaea44bda763038d62
Banu Atav <atavbanu@gmail.com>
2021-08-06 12:37:43 +0200

    ADD main function scraper

---
+++ src/scraper/__init__.py
@@ -0,0 +1,20 @@
+import os
+from selenium import webdriver
+from linkedin_scraper import actions
+
+from src import email
+from .job_ads import JobAd
+
+
+def scrape_jobs():
+    job_details = email.extract()
+    driver = webdriver.Chrome(os.environ["CHROMEDRIVER"])
+    actions.login(
+        driver, os.environ["LINKEDIN_EMAIL"], os.environ["LINKEDIN_PW"])
+
+    ads = []
+    for i, link in enumerate([x["link"] for x in job_details]):
+        print(i)
+        ad_i = JobAd(driver=driver, link=link)
+        ads.append(ad_i)
+    driver.quit()
